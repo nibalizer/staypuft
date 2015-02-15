7509eb52f8b38874af14ff0bd255a4f0f0e7215d
Spencer Krum <nibz@spencerkrum.com>
2015-02-15 16:57:10 +0000

    example config

---
+++ example_config.py
@@ -0,0 +1,10 @@
+#This all assumes you've made the questionable decision to use
+#bitlbee instead of an xmpp bot. Oh you did? Me too!
+nick = 'StayPuft'
+handle = '@StayPuft'
+server = '127.0.0.1'
+port = 2122
+channel = '&bitlbee'
+log = 'chatlog'
+postup_messages = ['identify secretpassword']
+additional_channels = ['actualworkchannel']
