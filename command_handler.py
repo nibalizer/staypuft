3c0a5413a97d0997fa3b5e34cfa35db60de8fe76
Spencer Krum <nibz@spencerkrum.com>
2015-03-16 08:34:26 +0000

    handle pms

--- command_handler.py
+++ command_handler.py
@@ -10,6 +10,9 @@ commands_available['zs'] = zuulstatus
 from actions.puppettypes import puppet_type_ref
 commands_available['puppet type'] = puppet_type_ref
 
+from actions.pagerduty import get_vanguard, get_sentinel, get_schedule
+commands_available['vanguard'] = get_vanguard
+commands_available['sentinel'] = get_sentinel
 
 def bothelp(bot, user, channel, msg, config):
     msg = "Use {0}".format(",".join(commands_available.keys()))
@@ -19,6 +22,9 @@ commands_available['help'] = bothelp
 
 def command_handler(bot, user, channel, msg, config):
     stripped_msg = msg.lstrip()
+
+    if channel == bot.nickname:
+        channel = user
     for command in commands_available.keys():
         if stripped_msg.startswith(command):
             commands_available[command](bot, user, channel, msg, config)
